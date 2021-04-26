cf736ffa778bb118e7bac5bf71d6868a9a94c091
ji-park <qkrskdjf@naver.com>
2021-04-27 03:41:27 +0900

    user.js에 가상 속성과 pre(save)훅 추가

--- model/user.js
+++ model/user.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema(
             first_name: {
                 type: String,
                 required: true,
-                trim: true,
+                trim: true, //공백제거
             },
             last_name: {
                 type: String,
@@ -62,4 +62,28 @@ const userSchema = mongoose.Schema(
         timestamp: true
     }
 );
+
+userSchema.virtual("fulName").get(function(){
+    return `${this.name.first} ${this.name.last}`;
+})
+
+userSchema.pre("save", function(next){
+    let user = this;
+    if (user.subscriberAccount === undefined)
+        Subscriber.findOne({
+            email : user.email
+        })
+        .then(subscriber => {
+            user.subscriberAccount = subscriber;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error in connecting subscriber: ${error.message}`);
+            next(error);
+        });
+        else {
+            next();
+        }
+});
+
 module.exports = mongoos.model("User", userSchema);
