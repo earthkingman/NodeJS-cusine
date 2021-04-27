b6b91bebd550b6bb896b40c5f03f9d1772d38dae
ji-park <qkrskdjf@naver.com>
2021-04-27 15:49:52 +0900

    구독자 라우트 구조화

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const Subscriber = require("./model/subscriber.js")
 const methodOverride = require("method-override");
 const mongoose = require("mongoose"); // 몽구스 요청
 const layouts = require("express-ejs-layouts"); // express-ejs-layout의 요청
-
+const express = require('express');  //express 요청
+const router = express.Router(); //라우터 추가
 mongoose.connect("mongodb://localhost:27017/recipe_db",  // 데이터베이스 연결 설정
     {
         useNewUrlParser: true,
@@ -19,9 +20,9 @@ db.once("open", () => {
 })
 
 const port = 3000;
-const express = require('express');  //express 요청
-app = express();   //express 애플리케이션의 인스턴스화
 
+app = express();   //express 애플리케이션의 인스턴스화
+app.use("/", router);
 app.use(methodOverride("_method", { method: ["POST", "GET"] }));
 app.set("view engine", "ejs"); // ejs를 사용하기 위한 애플리케이션 세팅
 app.use(layouts); //layout 모듈 사용을 위한 애플리케이션 세팅 app.use-> HTTP 메소드를 사용한 모든 요청에서 실행됨
@@ -33,6 +34,14 @@ app.use(express.static("./public")) //정적 에셋 제공
 app.get('/', (req, res) => {
     res.render("index");
 });
+
+router.get("/subscribers", subscriberController.index, subscriberController.indexView);
+router.get("/subscribers/new", subscriberController.new);
+router.post("/subscribers/create", subscriberController.create, subscriberController.redirectView);
+router.get("/subscribers:id", subscriberController.show, subscriberController.showView);
+router.get("/subscribers/:id/edit", subscriberController.edit);
+router.put("/subscirbers/:id/update", subscriberController.update, subscriberController.redirectView);
+router.delete("/subscribers/:id/delete", subscriberController.delete, subscriberController.redirectView);
 
 app.get("/course", homeController.showCourse); //코스 페이지, 연락처 페이지, 연락처 제출 양식을 위한 라우트의 추가
 app.get("/contact", homeController.showSignUp);
